d1f9cc2bbf8881f33c6832105bd737d3b94af5fe
Shyam Jayaraman <bjornick@google.com>
2014-04-25 15:03:18 -0700

    javascript/api/proxy: Added a bind cache to javascript.
    Change-Id: I50c03afa823d7476096b4a81ef344531e474fbd1

--- test/integration/client/signature_go_server.js
+++ test/integration/client/signature_go_server.js
@@ -32,7 +32,6 @@ describe('Signature, when service is in GO, getServiceSignature', function() {
 
   var signature;
   beforeEach(function(done) {
-
     var veyron = new Veyron(TestHelper.veyronConfig);
     var client = veyron.newClient();
 
@@ -57,7 +56,6 @@ describe('Signature, when service is in GO, getServiceSignature', function() {
   });
 
   it('Should be able to get number of in params for each method', function() {
-
     for (var methodName in expectedSignature) {
       if(expectedSignature.hasOwnProperty(methodName)) {
         var methodInfo = signature[methodName];
@@ -70,7 +68,6 @@ describe('Signature, when service is in GO, getServiceSignature', function() {
   });
 
   it('Should be able to get name of in params for each method', function() {
-
     for (var methodName in expectedSignature) {
       if(expectedSignature.hasOwnProperty(methodName)) {
         var methodInfo = signature[methodName];
@@ -85,7 +82,6 @@ describe('Signature, when service is in GO, getServiceSignature', function() {
   });
 
   it('Should be able to get number of out params for each method', function() {
-
     for (var methodName in expectedSignature) {
       if(expectedSignature.hasOwnProperty(methodName)) {
         var methodInfo = signature[methodName];
@@ -98,7 +94,6 @@ describe('Signature, when service is in GO, getServiceSignature', function() {
   });
 
   it('Should be able to tell if streaming for each method', function() {
-
     for (var methodName in expectedSignature) {
       if(expectedSignature.hasOwnProperty(methodName)) {
         var methodInfo = signature[methodName];
@@ -110,4 +105,70 @@ describe('Signature, when service is in GO, getServiceSignature', function() {
     }
   });
 
+  it('Should use a valid cache entry', function(done) {
+    var veyron = new Veyron(TestHelper.veyronConfig);
+    var client = veyron.newClient();
+
+    var absoluteVeyronName =
+        '/' + testconfig['SAMPLE_VEYRON_GO_SERVICE_ENDPOINT'] + '/cache';
+    client._proxyConnection.bindCache[absoluteVeyronName] = {
+      fetched: new Date(),
+      signature: {
+        'bar': 'foo'
+      }
+    };
+    client._proxyConnection.getServiceSignature(absoluteVeyronName)
+    .then(function(sig) {
+      expect(sig.bar).to.equal('foo');
+      done();
+    }).catch (done);
+  });
+
+  it('Should skip a stale cache entry', function(done) {
+    var veyron = new Veyron(TestHelper.veyronConfig);
+    var client = veyron.newClient();
+
+    var absoluteVeyronName =
+        '/' + testconfig['SAMPLE_VEYRON_GO_SERVICE_ENDPOINT'] + '/cache';
+    client._proxyConnection.bindCache[absoluteVeyronName] = {
+      // 3 hours ago.
+      fetched: new Date() - 7200 * 1000,
+      signature: {
+        'bar': 'foo'
+      }
+    };
+    client._proxyConnection.getServiceSignature(absoluteVeyronName)
+    .then(function(signature) {
+      expect(signature.bar).to.not.exists;
+      for (var methodName in expectedSignature) {
+        if(expectedSignature.hasOwnProperty(methodName)) {
+          var methodInfo = signature[methodName];
+          expect(methodInfo).to.exists;
+        }
+      }
+      done();
+    }).catch (done);
+  });
+
+  it('Should store a valid cache entry', function(done) {
+    var veyron = new Veyron(TestHelper.veyronConfig);
+    var client = veyron.newClient();
+
+    var absoluteVeyronName =
+        '/' + testconfig['SAMPLE_VEYRON_GO_SERVICE_ENDPOINT'] + '/cache';
+    client._proxyConnection.getServiceSignature(absoluteVeyronName)
+    .then(function() {
+      var entry = client._proxyConnection.bindCache[absoluteVeyronName];
+      var signature = entry.signature;
+      // Expect the timestamp to be recent.
+      expect(new Date() - entry.fetched).to.be.below(2000);
+      for (var methodName in expectedSignature) {
+        if(expectedSignature.hasOwnProperty(methodName)) {
+          var methodInfo = signature[methodName];
+          expect(methodInfo).to.exists;
+        }
+      }
+      done();
+    }).catch (done);
+  });
 });
