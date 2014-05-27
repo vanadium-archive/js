2ca94f37a799d9a6fde0e8f8bacd8dc1ddb79289
Shyam Jayaraman <bjornick@google.com>
2014-05-27 13:56:07 -0700

    veyron/javacript/api: Changed the setup to refer to the veyron proxy by veyron name instead of host port. This also was a convenient time to refactor the whole managing of the process.  We now use promises to create and manage the dependencies.
    Change-Id: I9376083a84d7c15f63975bb234168d66e65d61b2

--- grunt_tasks/integration_test_environment_setup.js
+++ grunt_tasks/integration_test_environment_setup.js
@@ -1,3 +1,9 @@
+var Promise = require('es6-promise').Promise;
+var deferred =  require('../src/lib/deferred');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
 /**
  * Provides two tasks for setup and tear down of integration tests. For instance
  * processes like wspr can be spawned & then killed in setup and teardown
@@ -7,144 +13,112 @@ module.exports = function(grunt) {
   // List of processes we have spawned so we can kill them on teardown
   var runningChildProcesses = [];
   var cleaningUp;
-
-  var numProcessesWaitingToStart;
-
-  var resetNumWaitingProcesses = function() {
-    // There are five processes that need to be started:
-    // the proxy, wspr, mounttable, identity, and the sample server
-    numProcessesWaitingToStart = 5;
-  }
-  var addListeners = function(done, runningProcess) {
+  var addListeners = function(runningProcess, resolve, reject) {
     var stderr = '';
-    runningProcess.stderr.once('data', function() {
-      numProcessesWaitingToStart--;
-      if (numProcessesWaitingToStart === 0) {
-        done(true);
-      }
-    });
+    if (resolve) {
+      runningProcess.stderr.once('data', function() {
+        resolve();
+      });
+    }
     runningProcess.stderr.on('data', function(d) {
       stderr += d;
       grunt.log.debug(d);
     });
     runningProcess.on('exit', function (code) {
       if (!cleaningUp) {
-        fail(runningProcess.title +
+        reject(runningProcess.title +
           ' crashed before tests finish with code: ' + code +
           ' here is the stderr:\n' + stderr);
       }
-     });
+    });
+  };
+
+  // Constants
+  // Binary location of wspr and other processes to run
+  var VEYRON_BIN_DIR = path.resolve('../../go/bin');
+  var VEYRON_PROXY_BIN = VEYRON_BIN_DIR + '/proxyd';
+  var MOUNTTABLE_BIN = VEYRON_BIN_DIR + '/mounttabled';
+  var WSPR_BIN = VEYRON_BIN_DIR + '/wsprd';
+  var IDENTITYD_BIN = VEYRON_BIN_DIR + '/identityd';
+  var SAMPLE_GO_SERVICE_BIN = VEYRON_BIN_DIR + '/sampled';
+
+  var BINARIES = [VEYRON_PROXY_BIN, MOUNTTABLE_BIN, WSPR_BIN, IDENTITYD_BIN,
+   SAMPLE_GO_SERVICE_BIN];
+
+  var WSPR_PORT = 3224; // The port for WSPR.
+  var IDENTITYD_PORT = 8125; // The port for the identityd server.
+
+  var LOGS_DIR = path.resolve('logs');
+
+
+  /**
+   * Starts a process that depends on the mounttable root and returns
+   * a handle the process.
+   * @param {string} mounttableRoot the veyron endpoint of the mounttable.
+   * @param {string} bin the path to the binary.
+   * @param {array} extraArgs an array of args that are binary specific. The
+   * logging flags will already be set.
+   * @return {Process} the process handle for the command.
+   */
+  var startProcessWithMounttableRoot = function(mounttableRoot, bin, name,
+   extraArgs) {
+    var envWithMount = {
+      VEYRON_IDENTITY: process.env.IDENTITY_FILE,
+      MOUNTTABLE_ROOT: mounttableRoot,
+    };
+    var args = ['-v=3', '-vv=3', '-log_dir=' + LOGS_DIR];
+    args.concat(extraArgs);
+    // Run process
+    var runningProcess = spawn(bin, extraArgs, { env: envWithMount});
+    runningProcess.title = name;
+    runningChildProcesses.push(runningProcess);
+    return runningProcess;
   };
 
   /**
-   * Setups the required environment for integration testing such as
-   * running wspr and sample Veyron services written in Go that are
-   * expected in the integration tests.
+   * Starts a process that depends on the mounttable root and returns
+   * a promise that will be resolved when the process has started.
+   * @param {string} mounttableRoot the veyron endpoint of the mounttable.
+   * @param {string} bin the path to the binary.
+   * @param {array} extraArgs an array of args that are binary specific. The
+   * logging flags will already be set.
+   * @return {Promise} a promise that will be resolved when the process has
+   * been started.
    */
-  grunt.registerTask('subtask_setupIntegrationTestEnvironment', function() {
-    var spawn = require('child_process').spawn;
-    var fs = require('fs');
-    var path = require('path');
+  var startProcessWithMounttableAndAddHandlers = function(mounttableRoot, bin,
+   name, extraArgs) {
+    var process = startProcessWithMounttableRoot(mounttableRoot, bin, name,
+     extraArgs);
+    var def = new deferred();
+    addListeners(process, def.resolve, def.reject);
+    return def.promise;
+  };
 
-    resetNumWaitingProcesses();
-    // Indicate this is a async grunt task
-    var done = this.async();
+  // Starts WSPR taking in the mounttable root and returns a promise that
+  // will be resolved when WSPR is ready to accept requests.
+  var startWSPR = function(mounttable) {
+    return startProcessWithMounttableAndAddHandlers(
+      mounttable, WSPR_BIN,
+      'WSPR', ['-port=' + WSPR_PORT, '-vproxy=test/proxy']);
+  };
 
-    var boundAddListeners = addListeners.bind(undefined, done);
+  // Starts the Veyron Proxy taking in the mounttable root and returns
+  // a promise that will be resolved when the proxy. is ready to accept requests.
+  var startProxy = function(mounttable) {
+    return startProcessWithMounttableAndAddHandlers(
+      mounttable, VEYRON_PROXY_BIN, 'Veyron Proxy',
+      ['-address=127.0.0.1:0', '-name=test/proxy']);
+  };
 
-    // Reset state
-    runningChildProcesses = [];
-    cleaningUp = false;
-
-    // Constants
-    // Binary location of wspr and other processes to run
-    var VEYRON_BIN_DIR = path.resolve('../../go/bin');
-    var VEYRON_PROXY_BIN = VEYRON_BIN_DIR + '/proxyd';
-    var MOUNTTABLE_BIN = VEYRON_BIN_DIR + '/mounttabled';
-    var WSPR_BIN = VEYRON_BIN_DIR + '/wsprd';
-    var IDENTITYD_BIN = VEYRON_BIN_DIR + '/identityd';
-    var SAMPLE_GO_SERVICE_BIN = VEYRON_BIN_DIR + '/sampled';
-
-    var VEYRON_PROXY_PORT = 3111;
-    var WSPR_PORT = 3224; // The port for WSPR.
-    var IDENTITYD_PORT = 8125; // The port for the identityd server.
-
-    var LOGS_DIR = path.resolve('logs');
-
-    // Ensure binaries exist
-    if (!fs.existsSync(WSPR_BIN) || !fs.existsSync(IDENTITYD_BIN) ||
-       !fs.existsSync(SAMPLE_GO_SERVICE_BIN)) {
-
-      var errorMessage = 'Veyron binaries not found. Ensure "veyron" and ' +
-        '"veyron2" are built and installed in ' + VEYRON_BIN_DIR + ' by ' +
-        'running go-amd64 install \n' +
-        WSPR_BIN + ', ' + IDENTITYD_BIN + ' and ' +
-        SAMPLE_GO_SERVICE_BIN + ' are required for integration testing';
-
-      fail(errorMessage);
-    }
-
-    var basicEnv = {
-      VEYRON_IDENTITY: process.env.IDENTITY_FILE
-    };
-    var mounttable_process = spawn(MOUNTTABLE_BIN,
-      ['-log_dir=' + LOGS_DIR, '-address=127.0.0.1:0'], {
-        env: basicEnv
-      });
-    mounttable_process.title = 'MountTable';
-    var endpointRe = /@.*@@\S+/;
-    var mounttableEndpoint = '';
-    mounttable_process.stderr.on('data', function(data) {
-      var string = data.toString();
-      if (mounttableEndpoint !== '') {
-        return;
-      }
-      var match = string.match(endpointRe);
-      if (!match) {
-        return
-      }
-      mounttableEndpoint = match[0];
-      var envWithMount = {
-        VEYRON_IDENTITY: process.env.IDENTITY_FILE,
-        MOUNTTABLE_ROOT: '/' + mounttableEndpoint,
-      };
-      // Run wspr
-      var wspr_process = spawn(WSPR_BIN,
-        ['-v=3', '-vv=3', '-log_dir=' + LOGS_DIR, '-port=' + WSPR_PORT,
-         '-vproxy=127.0.0.1:' + VEYRON_PROXY_PORT ], {
-          env: envWithMount
-        });
-      wspr_process.title = 'WSPR';
-      runningChildProcesses.push(wspr_process);
-      boundAddListeners(wspr_process);
-    });
-
-    runningChildProcesses.push(mounttable_process);
-    var veyron_proxy_process = spawn(VEYRON_PROXY_BIN,
-      ['-log_dir=' + LOGS_DIR, '-address=127.0.0.1:' + VEYRON_PROXY_PORT], {
-        env: basicEnv
-      });
-    veyron_proxy_process.title = 'Veyron Proxy';
-    runningChildProcesses.push(veyron_proxy_process);
-
-    // Run identityd
-    var identityd_process = spawn(IDENTITYD_BIN, ['-port=' + IDENTITYD_PORT], {
-      env: basicEnv
-    });
-    identityd_process.title = 'Identity Server';
-    runningChildProcesses.push(identityd_process);
-
-    // Run the sample go service proxy, we use the sampled example service
-    var sample_service_process = spawn(SAMPLE_GO_SERVICE_BIN,
-      ['-v=3', '-vv=3', '-log_dir=' + LOGS_DIR], {
-        env: basicEnv
-      });
-    sample_service_process.title = 'Sampled';
-    runningChildProcesses.push(sample_service_process);
-
+  // Starts sampled taking in the mounttable root and returns a ppromise
+  // that will resolved when sampled is up and running.
+  var startSampled = function(mounttable) {
+    var process =  startProcessWithMounttableRoot(
+      mounttable, SAMPLE_GO_SERVICE_BIN, 'Sampled', []);
+    var def = new deferred();
+    addListeners(process, null, def.reject);
     // Wait until we get the sample service endpoint from the process stdout
-    sample_service_process.stdout.on('data', function(data) {
-
+    process.stdout.on('data', function(data) {
       var endpoint = data.toString().replace(('Listening at: '), '').trim();
       if (!endpoint.match(/^@.*@$/)) {
         var errorMessage = SAMPLE_GO_SERVICE_BIN + ' did not print the ' +
@@ -164,9 +138,105 @@ module.exports = function(grunt) {
         IDENTITYD_PORT + '/random/';
 
       testConfigs['SAMPLE_VEYRON_GO_SERVICE_ENDPOINT'] = endpoint;
+      def.resolve();
+    });
+    return def.promise;
+  };
+
+  var startMounttable = function() {
+    var mounttableDef = new deferred();
+    var mounttablePromise = mounttableDef.promise;
+
+    var mounttable_process = spawn(MOUNTTABLE_BIN,
+      ['-log_dir=' + LOGS_DIR, '-address=127.0.0.1:0'], {
+        env: {
+          VEYRON_IDENTITY: process.env.IDENTITY_FILE
+        }
+      });
+    mounttable_process.title = 'MountTable';
+    var endpointRe = /@.*@@\S+/;
+    var mounttableEndpoint = '';
+    mounttable_process.stderr.on('data', function(data) {
+      var string = data.toString();
+      if (mounttableEndpoint !== '') {
+        return;
+      }
+      var match = string.match(endpointRe);
+      if (!match) {
+        return
+      }
+      mounttableEndpoint = match[0];
+      mounttableDef.resolve('/' + mounttableEndpoint);
     });
 
-    runningChildProcesses.forEach(boundAddListeners);
+    runningChildProcesses.push(mounttable_process);
+    addListeners(mounttable_process, null, mounttableDef.reject);
+    return mounttablePromise;
+  };
+
+  var startIdentityd = function() {
+    var identityd_process = spawn(IDENTITYD_BIN, ['-port=' + IDENTITYD_PORT], {
+      env: {
+          VEYRON_IDENTITY: process.env.IDENTITY_FILE
+        }
+    });
+    identityd_process.title = 'Identity Server';
+    runningChildProcesses.push(identityd_process);
+    var identityDef = new deferred();
+    addListeners(identityd_process, identityDef.resolve, identityDef.reject);
+    return identityDef.promise;
+  };
+  /**
+   * Setups the required environment for integration testing such as
+   * running wspr and sample Veyron services written in Go that are
+   * expected in the integration tests.
+   */
+  grunt.registerTask('subtask_setupIntegrationTestEnvironment', function() {
+    // Indicate this is a async grunt task
+    var done = this.async();
+
+    // Reset state
+    runningChildProcesses = [];
+    cleaningUp = false;
+
+    // Ensure binaries exist
+    BINARIES.forEach(function(bin) {
+      if (!fs.existsSync(bin)) {
+        var errorMessage = 'Veyron binaries not found. Ensure "veyron" and ' +
+        '"veyron2" are built and installed in ' + VEYRON_BIN_DIR + ' by ' +
+        'running go-amd64 install\n The following binaries are needed:\n' +
+        BINARIES.join('\n');
+        fail(errorMessage);
+      }
+    });
+
+    var basicEnv = {
+      VEYRON_IDENTITY: process.env.IDENTITY_FILE
+    };
+
+    var servicePromises = [];
+    var mounttablePromise = startMounttable();
+    servicePromises.push(mounttablePromise);
+
+    servicePromises.push(startIdentityd());
+
+    servicePromises.push(mounttablePromise.then(startWSPR));
+
+    servicePromises.push(mounttablePromise.then(startProxy));
+
+    servicePromises.push(mounttablePromise.then(startSampled));
+
+    Promise.all(servicePromises).then(function() {
+      if (BINARIES.length != runningChildProcesses.length) {
+        fail('Not all binaries were started');
+        done(false);
+      } else {
+        done(true);
+      }
+    }).catch(function(err) {
+      fail(err);
+      done(false);
+    });
   });
 
   // Kills any running process we started and calls done when all finish
