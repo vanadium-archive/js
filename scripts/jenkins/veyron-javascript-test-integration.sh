8198159b0c61fe4c44d1f8e7b854c27c41c1306b
Nicolas LaCasse <nlacasse@google.com>
2014-08-12 16:22:23 -0700

    Fix jenkins js tests.  The tests were running without the necessary browser test bundles being built.
    Change-Id: I4a0f28f2fee0c53872c303b45214d8fed507c474

--- scripts/jenkins/veyron-javascript-test-integration.sh
+++ scripts/jenkins/veyron-javascript-test-integration.sh
@@ -9,6 +9,7 @@ mkdir -p "${TMPDIR}"
 readonly REPO_ROOT="$(git rev-parse --show-toplevel)"
 "${VEYRON_ROOT}/veyron/scripts/build/go" install veyron/... veyron2/...
 cd "${REPO_ROOT}"
+make test_out/veyron.test.integration.js
 xvfb-run -a -s "-screen -0 800x600x24" ./vgrunt jenkins_tests --tests integration
 
 rm -rf "${OUTDIR}"
