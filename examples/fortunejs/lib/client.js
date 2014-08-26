36e06ad26ff7fa8bed2a9415d41eca1fd20d9b2a
Jeffrey Regan <git-jregan.google.com>
2014-08-26 11:15:44 -0700

    veyron/veyron.js/examples/fortunejs: some cleanups
    Replaced 'if' statements in fortunejs client with a polymorph,
    in attempt to make it easier to read.
    
    Removed some code and comments that confused me.
    
    Hand tested both the node and browser client, both still work.
    
    Change-Id: Ib50c319cd24b36cf4f0ad1190e8fbc71ca3bdb4b

--- examples/fortunejs/lib/client.js
+++ examples/fortunejs/lib/client.js
@@ -1,85 +1,97 @@
 var veyron = require('../../../src/veyron');
 var veyronConfig = require('./config');
 
-var isBrowser = (typeof window === 'object');
-
-// In NodeJS, parse the command line argument to see if we are adding or
-// requesting a fortune.
-var argv;
-if (!isBrowser) {
-  argv = require('optimist').argv;
-  if (!argv.add) {
-    console.log('To add a new fortune use with --add "new fortune text" arguments\n');
-  }
-}
-
-console.log('***Welcome to FortuneJS - Client***');
-
 /**
- * Bind to the fortune server.
+ * Create a Veyron runtime using the configuration defined in config.js,
+ * and bind it to the bakery/cookie/fortune service.
  */
-
-// Create a Veyron runtime using the configuration defined in config.js.
 veyron.init(veyronConfig).then(function(rt){
-  var fortuneServiceName = 'bakery/cookie/fortune';
-  var fortuneService = rt.bindTo(fortuneServiceName);
-
-  // Bind to the service. Bind returns a Promise object. When fortune object is
-  // bound, use it like a regular JavaScript object.
-  fortuneService.then(function(fortuneService) {
-
-    // Get a new Fortune to display.
+  rt.bindTo('bakery/cookie/fortune').then(function(fortuneService) {
     function getFortune() {
       fortuneService.getRandomFortune().then(function(fortune) {
-        // Display is different for Browser and NodeJS.
-        if (isBrowser) {
-          // Browser should display the fortune in the #fortuneText div.
-          document.querySelector('#fortuneText').textContent = fortune;
-        } else {
-          // Node should just console.log the fortune and exit.
-          console.log('Your fortune:', fortune);
-          process.exit(0);
-        }
+        context.handleGetSuccess(fortune);
       }).catch(function(err) {
-          console.error('Better days lay ahead! Sorry, we encountered an error :(', err);
+        context.handleGetFailure(err);
       });
     }
 
-    function addNewFortune() {
-      var fortune;
-      if (isBrowser) {
-        // Read fortune from input value.
-        fortune = document.querySelector('#fortuneInput').value;
-        document.querySelector('#fortuneInput').value = '';
-      } else {
-        // Fortune comes from command line argument.
-        fortune = argv.add;
+    function addFortune() {
+      fortuneService.addNewFortune(context.getFortuneToAdd()).then(function() {
+        context.handleAddSuccess();
+      }).catch(function(err) {
+        context.handleAddFailure(err);
+      });
+    }
+
+    var browserContext = {
+      start: function() {
+        // Load JS to handle browser client style.
+        require('./client-style');
+        // Hook up browser click events.
+        window.document.querySelector(
+            '#newFortune').addEventListener('click', getFortune);
+        window.document.querySelector(
+            '#saveFortune').addEventListener('click', addFortune);
+        // Browser should start by doing a 'get'.
+        getFortune();
+      },
+      getFortuneToAdd: function() {
+        var text = window.document.querySelector('#fortuneInput').value;
+        window.document.querySelector('#fortuneInput').value = '';
+        return text;
+      },
+      handleGetSuccess: function(text) {
+        window.document.querySelector('#fortuneText').textContent = text;
+      },
+      // TODO: Errors should show up in the web page.
+      handleGetFailure: function(err) {
+        console.error('Get failure: ', err);
+      },
+      handleAddSuccess: function() {
+        console.info('New fortune successfully added.');
+      },
+      handleAddFailure: function(err) {
+        console.error('Add failure: ', err);
       }
+    };
 
-      fortuneService.addNewFortune(fortune).then(function() {
-        console.info('New fortune successfully added');
-        if (!isBrowser) { process.exit(0); }
-      }).catch(function(err) {
-        console.error('Failed to add new fortune because: ', err);
-        if (!isBrowser) { process.exit(1); }
-      });
-    }
+    var nodeContext = {
+      fortuneText: '',
+      start: function() {
+        // Parse command line to see if this is an add or a get.
+        var argv = require('optimist').argv;
+        if (argv.add) {
+          this.fortuneText = argv.add;
+          addFortune();
+          return;
+        }
+        console.log('To add a fortune, run with:  --add "new fortune text"\n');
+        getFortune();
+      },
+      getFortuneToAdd: function() {
+        return this.fortuneText;
+      },
+      handleGetSuccess: function(text) {
+        console.log(text);
+        process.exit(0);
+      },
+      handleGetFailure: function(err) {
+        console.error('Get failure: ', err);
+        process.exit(1);
+      },
+      handleAddSuccess: function() {
+        console.info('New fortune successfully added.');
+        process.exit(0);
+      },
+      handleAddFailure: function(err) {
+        console.error('Add failure: ', err);
+        process.exit(1);
+      }
+    };
 
-    if (isBrowser) {
-      // Load JS to handle browser client style.
-      require('./client-style');
-
-      // Hook up browser click events.
-      document.querySelector('#newFortune').addEventListener('click', getFortune);
-      document.querySelector('#saveFortune').addEventListener('click', addNewFortune);
-    }
-
-    // If we are adding a fortune, add it, otherwise get a new fortune.
-    if (argv && argv.add) {
-      addNewFortune();
-    } else {
-      getFortune();
-    }
+    console.log('*** Welcome to FortuneJS - Client ***');
+    var context = (typeof window === 'object') ? browserContext : nodeContext;
+    context.start();
   });
 }).catch(function(err) {
   console.error('Failed to use the fortune service because:', err);
