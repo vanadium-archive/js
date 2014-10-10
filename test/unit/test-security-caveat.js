a74e917763b7cd56a1c63e99cc9cae755b7950a3
Asim Shankar <ashankar@google.com>
2014-10-09 19:20:52 -0700

    security/caveat: Remove the PeerBlessingsCaveat.
    Reference:
    https://veyron-review.googlesource.com/#/c/5467/
    
    Change-Id: I70efbf5cf0c6b02bc1c57de44f57e2055a3a88c3

--- test/unit/test-security-caveat.js
+++ test/unit/test-security-caveat.js
@@ -1,7 +1,6 @@
 var test = require('prova');
 var caveat = require('../../src/security/caveat.js');
 var MethodCaveat = caveat.MethodCaveat;
-var PeerBlessingsCaveat = caveat.PeerBlessingsCaveat;
 
 test('var caveat = new MethodCaveat(methods)', function(assert) {
   var methods = [ 'Enter', 'Leave' ];
@@ -12,20 +11,6 @@ test('var caveat = new MethodCaveat(methods)', function(assert) {
   assert.deepEqual(json, {
     _type: 'MethodCaveat',
     data: methods
-  });
-
-  assert.end();
-});
-
-test('var caveat = new PeerBlessingsCaveat(patterns)', function(assert) {
-  var patterns = [ 'veyron/batman', 'veyron/brucewayne' ];
-  var caveat = new PeerBlessingsCaveat(patterns);
-  var string = JSON.stringify(caveat);
-  var json = JSON.parse(string);
-
-  assert.deepEqual(json, {
-    _type: 'PeerBlessingsCaveat',
-    data: patterns
   });
 
   assert.end();
