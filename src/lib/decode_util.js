8818da63396adb559011beed062e1da0362d0ef7
Benjamin Prosnitz <bprosnitz@google.com>
2014-11-24 13:56:36 +0800

    TBR Recursively unwrap all decoded values
    This is a temporary fix to reduce a significant amount of work
    handling wrapped objects until the final format is decided on
    
    Change-Id: Iebff7bb358ef4efdbde0b7e8f72c715b1e8b9324

--- src/lib/decode_util.js
+++ src/lib/decode_util.js
@@ -6,5 +6,8 @@ module.exports = {
 function tryDecode(hex) {
   var reader = new vom.ByteArrayMessageReader(vom.Util.hex2Bytes(hex));
   var decoder = new vom.Decoder(reader);
-  return decoder.decode();
+  var decoded = decoder.decode();
+  // TODO(bprosnitz) Remove unwrapping.
+  var unwrapped = vom.TypeUtil.recursiveUnwrap(decoded);
+  return unwrapped;
 }
