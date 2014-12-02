6f6b6aaf85abf77a012c8aeae6884763fd4c7fd3
Todd Wang <toddw@google.com>
2014-12-02 15:26:00 -0800

    veyron.js: Fix trivial line length > 80 issue.
    Believe it or not, lines > 80 chars breaks veyron-javascript-vdl
    
    http://www.envyor.com/jenkins/job/veyron-javascript-vdl/80/console
    
    Change-Id: Id70b5a39e645ae83ae74b4c8b6cb9bb9121efd4d

--- src/invocation/wrapper.js
+++ src/invocation/wrapper.js
@@ -54,8 +54,8 @@ function wrapService(service, desc) {
   * Translates args to the native list-of-injections-and-non-injections style.
   * @param {int[]} argOffsets List of original indicies of args. See
   * getArgOffsets().
-  * @param {Object.<string,number>} injectionPositions Map from injection name to
-  * position.
+  * @param {Object.<string,number>} injectionPositions Map from injection name
+  * to position.
   * @param {Object[]} args List of args coming off of the wire.
   * @param {Object.<string,Object>} Object containing possible injection values.
   * @return {string[]} A list of combined injections and args off the wire.
@@ -85,8 +85,8 @@ function translateToTrueCallArgs(argOffsets, injectionPositions, args,
   * @param {Function} method The method to invoke
   * @param {int[]} argOffsets List of original indicies of args. See
   * getArgOffsets().
-  * @param {Object.<string,number>} injectionPositions Map from injection name to
-  * position.
+  * @param {Object.<string,number>} injectionPositions Map from injection name
+  * to position.
   * @param {Object[]} args List of args coming off of the wire.
   * @param {Object.<string,Object>} Object containing possible injection values.
   * @return {string[]} A list of combined injections and args off the wire.
