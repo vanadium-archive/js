9e598f5cd5df1d62fc50b342a7b3afd7876d4c96
Nicolas LaCasse <nlacasse@google.com>
2015-02-21 14:40:03 -0800

    Remove 'crypto' library from browser dependency.
    The crypto library was adding ~1.1M to our bundle.  We were only using
    it for crypto.randomBytes.  I re-wrote that method to use
    window.getRandomValues directly.
    
    Change-Id: I109625228a13d04a4f2d67c6487e854fb2af771f

--- src/lib/random.js
+++ src/lib/random.js
@@ -3,19 +3,36 @@
  * @private
  */
 
-// This will use window.crypto in browser, and node's crypto library in node.
-var crypto = require('crypto');
+var Buffer = require('buffer').Buffer;
+var isBrowser = require('is-browser');
 
 module.exports = {
   int32: int32,
   hex: hex
 };
 
+var crypto;
+
+// This will use window.crypto in browser, and node's crypto library in node.
+function randomBytes(len) {
+  len = len || 1;
+  if (isBrowser) {
+    var array = new Int8Array(len);
+    window.crypto.getRandomValues(array);
+    return new Buffer(array);
+  }
+
+  // Lazily load crypto library.  We use an expression in argument to require so
+  // browserify won't automatically bundle large crypto library.
+  crypto = crypto || require('crypt' + 'o');
+  return crypto.randomBytes(len);
+}
+
 function int32() {
-  return crypto.randomBytes(4).readInt32BE(0);
+  return randomBytes(4).readInt32BE(0);
 }
 
 function hex(len) {
   len = len || 16;
-  return crypto.randomBytes(len/2).toString('hex');
+  return randomBytes(Math.ceil(len/2)).toString('hex').substr(0, len);
 }
---
+++ test/unit/test-random.js
@@ -0,0 +1,30 @@
+var test = require('prova');
+
+var random = require('../../src/lib/random');
+
+var hexChars = '0123456789abcdef';
+function isHexString(s) {
+  if (typeof s !== 'string') {
+    return false;
+  }
+  for (var i = 0; i < s.length; i++) {
+    if (hexChars.indexOf(s[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+test('random.int32()', function(t) {
+  t.ok(typeof random.int32() === 'number', 'returns a number');
+  t.end();
+});
+
+test('random.hex(l)', function(t) {
+  ([1, 2, 7, 8, 16, 100]).forEach(function(i) {
+    var s = random.hex(i);
+    t.ok(isHexString(s), 'returns a hex string');
+    t.ok(s.length === i, 'of length ' + i);
+  });
+  t.end();
+});
