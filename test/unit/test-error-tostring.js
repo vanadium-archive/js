1d75ae8d3612e9eb0409871166c1f18010205d70
Benjamin Prosnitz <bprosnitz@google.com>
2015-03-02 16:33:58 -0800

    Add test for toString() on VanadiumError
    Change-Id: I8085aef2b26c1649a1e79b5d9828030df836e9f9

---
+++ test/unit/test-error-tostring.js
@@ -0,0 +1,9 @@
+var test = require('prova');
+var verror = require('../../src/v.io/v23/verror');
+
+test('Working toString on BadArgError (a VanadiumError)', function(t) {
+  t.equal(new verror.BadArgError(null, 'an arg').toString(),
+    'Error: app:op: Bad argument: an arg',
+    'UnknownError has the expected toString value');
+  t.end();
+});
