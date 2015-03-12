01b950410fa5935f5325ed91f1e0a5baf6318c1d
Ali Ghassemi <aghassemi@google.com>
2015-03-12 16:45:09 -0700

    core.js: Implemeting splitAddressName in JS. Also removing stripBasename, it was not used and unclear what it did.
    closes https://github.com/veyron/release-issues/issues/1372
    
    Change-Id: I6773a2418364e4b4df679241a12602f4644de2a5

--- test/unit/test-namespace-util.js
+++ test/unit/test-namespace-util.js
@@ -1,6 +1,7 @@
 var test = require('prova');
 var names = require('../../src/namespace/util');
-var ep = '/@2@tcp@h:0@@@@@';
+var ep = '@4@tcp@127.0.0.1:22@@@@s@dev.v.io/blessing,v.io/blessing/b2@@';
+var rootedEp = '/' + ep;
 
 test('names.join(...)', function(assert) {
   assert.equal(names.join('a', 'b'), 'a/b');
@@ -21,30 +22,8 @@ test('names.join(...)', function(assert) {
   assert.equal(names.join(), '');
   assert.equal(names.join([]), '');
   assert.equal(names.join('/a//'), '/a');
-  assert.end();
-});
-
-test('names.isRooted(name)', function(assert) {
-  assert.ok(names.isRooted('/'));
-  assert.ok(names.isRooted('/a'));
-  assert.ok(names.isRooted('/a/b'));
-  assert.ok(names.isRooted(ep + '/'));
-  assert.notOk(names.isRooted(''));
-  assert.notOk(names.isRooted('a'));
-  assert.notOk(names.isRooted('b'));
-  assert.ok(names.isRooted('//a'));
-  assert.ok(names.isRooted('//' + ep));
-  assert.end();
-});
-
-test('names.clean(name)', function(assert) {
-  assert.equal(names.clean(''), '');
-  assert.equal(names.clean('a'), 'a');
-  assert.equal(names.clean('/b'), '/b');
-  assert.equal(names.clean('a/'), 'a');
-  assert.equal(names.clean('/b/'), '/b');
-  assert.equal(names.clean('//foo'), '/foo');
-  assert.equal(names.clean('//foo///bar/////'), '/foo/bar');
+  assert.equal(names.join(rootedEp, 'a'), rootedEp + '/a');
+  assert.equal(names.join(rootedEp + '/', '/a'), rootedEp + '/a');
   assert.end();
 });
 
@@ -59,6 +38,36 @@ test('names.stripBasename(name)', function(assert) {
   assert.equal(names.stripBasename('a/'), '');
   assert.equal(names.stripBasename('a/b'), 'a');
   assert.equal(names.stripBasename('a/b/'), 'a');
+  assert.equal(names.stripBasename(rootedEp), '');
+  assert.equal(names.stripBasename(rootedEp + '/a'), rootedEp);
+  assert.end();
+});
+
+test('names.isRooted(name)', function(assert) {
+  assert.ok(names.isRooted('/'));
+  assert.ok(names.isRooted('/a'));
+  assert.ok(names.isRooted('/a/b'));
+  assert.ok(names.isRooted(rootedEp + '/'));
+  assert.notOk(names.isRooted(''));
+  assert.notOk(names.isRooted('a'));
+  assert.notOk(names.isRooted('b'));
+  assert.ok(names.isRooted('//a'));
+  assert.ok(names.isRooted('//' + rootedEp));
+  assert.ok(names.isRooted(rootedEp));
+  assert.end();
+});
+
+test('names.clean(name)', function(assert) {
+  assert.equal(names.clean(''), '');
+  assert.equal(names.clean('a'), 'a');
+  assert.equal(names.clean('/b'), '/b');
+  assert.equal(names.clean('a/'), 'a');
+  assert.equal(names.clean('/b/'), '/b');
+  assert.equal(names.clean('//foo'), '/foo');
+  assert.equal(names.clean('//foo///bar/////'), '/foo/bar');
+  assert.equal(names.clean(rootedEp), rootedEp);
+  assert.equal(names.clean('//' + rootedEp), rootedEp);
+  assert.equal(names.clean(rootedEp + '//'), rootedEp);
   assert.end();
 });
 
@@ -73,5 +82,77 @@ test('names.basename(name)', function(assert) {
   assert.equal(names.basename('a/'), 'a');
   assert.equal(names.basename('a/b'), 'b');
   assert.equal(names.basename('a/b/'), 'b');
+  assert.end();
+});
+
+test('names.basename(name)', function(assert) {
+  assert.equal(names.basename('/'), '');
+  assert.equal(names.basename('/a'), 'a');
+  assert.equal(names.basename('/a/'), 'a');
+  assert.equal(names.basename('/a/b'), 'b');
+  assert.equal(names.basename('/a/b/'), 'b');
+  assert.equal(names.basename(''), '');
+  assert.equal(names.basename('a'), 'a');
+  assert.equal(names.basename('a/'), 'a');
+  assert.equal(names.basename('a/b'), 'b');
+  assert.equal(names.basename('a/b/'), 'b');
+  assert.equal(names.basename(rootedEp), ep);
+  assert.equal(names.basename(rootedEp + '/'), ep);
+  assert.equal(names.basename(rootedEp + '/b'), 'b');
+  assert.end();
+});
+
+test('names.splitAddressName(name)', function(assert) {
+  var tests = [
+    ['', '', ''],
+    ['/', '', ''],
+    ['//', '', ''],
+    ['//abc@@host/foo', 'abc@@host', 'foo'],
+    ['a', '', 'a'],
+    ['/a', 'a', ''],
+    ['/a/', 'a', ''],
+    ['a/b', '', 'a/b'],
+    ['/a/b', 'a', 'b'],
+    ['abc@@/foo', '', 'abc@@/foo'],
+    ['/abc@@host/foo', 'abc@@host', 'foo'],
+    ['/abc/foo', 'abc', 'foo'],
+    ['/abc/foo//x', 'abc', 'foo/x'],
+    ['/abc:20/foo', 'abc:20', 'foo'],
+    ['/abc//foo/bar', 'abc', 'foo/bar'],
+    ['/0abc:20/foo', '0abc:20', 'foo'],
+    ['/abc1.2:20/foo', 'abc1.2:20', 'foo'],
+    ['/abc:xx/foo', 'abc:xx', 'foo'],
+    ['/-abc/foo', '-abc', 'foo'],
+    ['/a.-abc/foo', 'a.-abc', 'foo'],
+    ['/[01:02::]:444', '[01:02::]:444', ''],
+    ['/[01:02::]:444/foo', '[01:02::]:444', 'foo'],
+    ['/12.3.4.5:444', '12.3.4.5:444', ''],
+    ['/12.3.4.5:444/foo', '12.3.4.5:444', 'foo'],
+    ['/12.3.4.5', '12.3.4.5', ''],
+    ['/12.3.4.5/foo', '12.3.4.5', 'foo'],
+    ['/12.3.4.5//foo', '12.3.4.5', 'foo'],
+    ['/12.3.4.5/foo//bar', '12.3.4.5', 'foo/bar'],
+    ['/user@domain.com@host:1234/foo/bar', 'user@domain.com@host:1234', 'foo/bar'], // jshint ignore:line
+    ['/(dev.v.io/services/mounttabled)@host:1234/foo/bar', '(dev.v.io/services/mounttabled)@host:1234', 'foo/bar'], // jshint ignore:line
+    ['/(dev.v.io/services/mounttabled)@host:1234/', '(dev.v.io/services/mounttabled)@host:1234', ''], // jshint ignore:line
+    ['/(dev.v.io/services/mounttabled)@host:1234', '(dev.v.io/services/mounttabled)@host:1234', ''], // jshint ignore:line
+    // the next two tests have malformed endpoint, doesn't end in a @@
+    ['/@4@tcp@127.0.0.1:22@@@@s@dev.v.io/', '@4@tcp@127.0.0.1:22@@@@s@dev.v.io', ''], // jshint ignore:line
+    ['/@4@tcp@127.0.0.1:22@@@@s@dev.v.io', '@4@tcp@127.0.0.1:22@@@@s@dev.v.io', ''],  // jshint ignore:line
+    ['/@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled@@/foo/bar', '@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled@@', 'foo/bar'],  // jshint ignore:line
+    ['/@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled,staging.v.io/services/nsroot@@/foo/bar', '@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled,staging.v.io/services/nsroot@@', 'foo/bar'],  // jshint ignore:line
+    ['/@@@127.0.0.1:22@@@@/foo/bar', '@@@127.0.0.1:22@@@@', 'foo/bar'], // jshint ignore:line
+    ['/@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled,staging.v.io/services/nsroot@@', '@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled,staging.v.io/services/nsroot@@', ''],  // jshint ignore:line
+    ['/@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled@@/foo/bar', '@4@tcp@127.0.0.1:22@@@@s@dev.v.io/services/mounttabled@@', 'foo/bar'],  // jshint ignore:line
+    [rootedEp, ep, ''],
+    [rootedEp + '/b', ep, 'b'],
+    [ep, '', ep],
+  ];
+  tests.forEach(function(t) {
+    assert.deepEqual(names.splitAddressName(t[0]),
+      { address: t[1], suffix: t[2] }
+    );
+  });
+
   assert.end();
 });
