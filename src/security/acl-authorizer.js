03510632bca2ba985765653b7e5fb29d0462251d
Shyam Jayaraman <bjornick@google.com>
2015-04-01 17:11:53 -0700

    js/core: changed acl authorizer to match the go style of authorization.
    Change-Id: Ifacbcac54e62ce5e3c6446480ec596ea2f68cc83

--- src/security/acl-authorizer.js
+++ src/security/acl-authorizer.js
@@ -1,16 +1,30 @@
 // Copyright 2015 The Vanadium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
 /**
  * @fileoverview The AccessList authorizer
  * @private
  */
 var blessingMatches = require('./blessing-matching');
-var vError = require('./../gen-vdl/v.io/v23/verror');
-var context = require('../runtime/context');
+var unwrap = require('../vdl/type-util').unwrap;
+var makeError = require('../errors/make-errors');
+var actions = require('../errors/actions');
+var NoPermissionsError =
+  require('../gen-vdl/v.io/v23/security/access').NoPermissionsError;
 
 module.exports = authorizer;
+var pkgPath = 'v.io/v23/security/access';
+var MultipleTagsError = makeError(
+  pkgPath + '.errMultipleMethodTags',
+  actions.NO_RETRY,
+  '{1:}{2:}PermissionsAuthorizer on {3}.{4} cannot handle multiple tags of ' +
+  'type {5} ({6}); this is likely unintentional{:_}');
+var NoTagsError = makeError(
+  pkgPath + '.errNoMethodTags',
+  actions.NO_RETRY,
+  '{1:}{2:}PermissionsAuthorizer.Authorize called with an object ({3}, ' +
+  'method {4}) that has no tags of type {5}; this is likely unintentional' +
+  '{:_}');
 
 /**
  * The AccessList authorizer.
@@ -18,9 +32,10 @@ module.exports = authorizer;
  * @memberof module:vanadium.security
  * @name aclAuthorizer
  * @param {module:vanadium.security.AccessList} acl The set of acls to apply.
+ * @param {constructor} type The type of tags that this authorizer understands.
  * @return {Authorize} An authorizer that applies the acls.
  */
-function authorizer(acl) {
+function authorizer(acl, type) {
   return function authorize(ctx) {
     // If the remoteBlessings is ourselves (i.e a self rpc), then we
     // always authorize.
@@ -28,61 +43,43 @@ function authorizer(acl) {
         ctx.localBlessings.publicKey === ctx.remoteBlessings.publicKey) {
       return null;
     }
-    var remoteNames = ctx.remoteBlessingStrings;
-    if ((remoteNames === undefined || remoteNames.length === 0) &&
-         canAccessAccessList('', ctx.label, acl)) {
-       return null;
+    var tags = ctx.methodTags.filter(function(t) {
+      return t instanceof type;
+    });
+    if (tags.length > 1) {
+      return new MultipleTagsError(null, ctx.suffix, ctx.methods, type._type,
+                                   ctx.methodTags);
     }
-    for (var i = 0; i < remoteNames.length; i++) {
-      if (canAccessAccessList(remoteNames[i], ctx.label, acl)) {
-        return null;
-      }
+
+    if (tags.length === 0) {
+      return new NoTagsError(null, ctx.suffix, ctx.methods, type._type,
+                             ctx.methodTags);
+
     }
-    // TODO(bjornick): find the right context.
-    return new vError.NoAccessError(new context.Context());
+
+    var key = unwrap(tags[0]);
+    var lists = acl.get(key);
+    if (!lists || !canAccess(ctx.remoteBlessingStrings, lists.in,
+                                lists.notIn)) {
+      return new NoPermissionsError(null, ctx.remoteBlessingStrings, [], key);
+    }
+    return null;
   };
 }
 
 // Returns whether name passed in has permission for the passed in
 // label.
-function canAccessAccessList(name, label, acl) {
-  // The set of labels that are allowed for
-  // the given names.
-  var pattern;
-  var patLabels;
-  var isAllowed = false;
-
-  // Add all the inclusive patterns.
-  for (pattern in acl.in) {
-    if (!acl.in.hasOwnProperty(pattern)) {
-      continue;
-    }
-    patLabels = acl.in[pattern];
-    if (blessingMatches(name, pattern)) {
-      if (patLabels.indexOf(label) !== -1) {
-        isAllowed = true;
-        break;
-      }
-    }
-  }
-
-  // If the names didn't match of the in patterns, then they are
-  // rejected.
-  if (!isAllowed) {
-    return false;
-  }
-
-  for (pattern in acl.notIn) {
-    if (!acl.notIn.hasOwnProperty(pattern)) {
-      continue;
-    }
-    patLabels = acl.notIn[pattern];
-    if (blessingMatches(name, pattern)) {
-      if (patLabels.indexOf(label) !== -1) {
-        return false;
-      }
-    }
-  }
-
-  return true;
+function canAccess(names, inSet, notInSet) {
+  // Remove the names that are blacklisted.
+  var unblacklistedNames = names.filter(function(name) {
+    return notInSet.every(function(pattern) {
+      return !blessingMatches(name, pattern);
+    });
+  });
+  // Check the remaining names for a match in the white list.
+  return unblacklistedNames.some(function(name) {
+    return inSet.some(function(pattern) {
+      return blessingMatches(name, pattern);
+    });
+  });
 }
--- test/unit/test-acl-authorizer.js
+++ test/unit/test-acl-authorizer.js
@@ -5,9 +5,15 @@
 
 var test = require('prova');
 var aclAuthorizer = require('../../src/security/acl-authorizer');
-var verror = require('../../src/gen-vdl/v.io/v23/verror');
-var Labels = require('../../src/security/labels');
+var access = require('../../src/gen-vdl/v.io/v23/security/access');
+var unwrap = require('../../src/vdl/type-util').unwrap;
+var createConstructor = require('../../src/vdl/create-constructor');
+var kind = require('../../src/vdl/kind');
 
+require('es6-shim');
+
+var allTags = [
+  access.Admin, access.Debug, access.Read, access.Write, access.Resolve];
 test('allow same public key access with no other acls', function(assert) {
   var ctx = {
     localBlessings: {
@@ -18,45 +24,48 @@ test('allow same public key access with no other acls', function(assert) {
     },
   };
 
-  var auth = aclAuthorizer({});
-  for (var l in Labels) {
-    if (!Labels.hasOwnProperty(l)) {
-      continue;
-    }
-    ctx.label = Labels[l];
+  var auth = aclAuthorizer({}, access.Tag);
+  allTags.forEach(function(t) {
+    ctx.methodTags = [t];
     assert.equal(auth(ctx), null);
-  }
+  });
 
   assert.end();
 });
 
 test('full suite of go tests', function(assert) {
-  var acl = {
-    in: {
-      '...': [Labels.READ],
-      'server/alice': [Labels.WRITE, Labels.READ],
-      'server/alice/$': [Labels.ADMIN, Labels.DEBUG, Labels.MONITORING],
-      'server/bob/$': [Labels.DEBUG, Labels.MONITORING],
-      'server/che': [Labels.WRITE, Labels.READ],
-      'server/che/$': [Labels.WRITE, Labels.READ],
-    },
-    notIn: {
-      'server/che/friend': [Labels.WRITE],
-    }
-  };
+  var acl = new Map();
+  acl.set(unwrap(access.Read), {
+    in: ['server/alice', 'server/che/$', '...'],
+    notIn: []
+  });
 
-  var auth = aclAuthorizer(acl);
+  acl.set(unwrap(access.Write), {
+    in: ['server/alice', 'server/che/$', 'server/che'],
+    notIn: [ 'server/che/friend' ]
+  });
+
+  acl.set(unwrap(access.Admin), {
+    in: ['server/alice/$'],
+    notIn: []
+  });
+
+  acl.set(unwrap(access.Debug), {
+    in: ['server/alice/$', 'server/bob'],
+    notIn: []
+  });
+
+  var auth = aclAuthorizer(acl, access.Tag);
   var expectations = {
-    'alice': [Labels.READ],
-    'bob': [Labels.READ],
-    'che': [Labels.READ],
-    'server/alice': [Labels.READ, Labels.WRITE, Labels.ADMIN, Labels.DEBUG,
-      Labels.MONITORING],
-    'server/bob': [Labels.READ, Labels.DEBUG, Labels.MONITORING],
-    'server/alice/friend': [Labels.READ, Labels.WRITE],
-    'server/che': [Labels.READ, Labels.WRITE],
-    'server/che/friend': [Labels.READ],
-    '': [Labels.READ],
+    'alice': [access.Read],
+    'bob': [access.Read],
+    'che': [access.Read],
+    'server/alice': [access.Read, access.Write, access.Admin, access.Debug],
+    'server/bob': [access.Read, access.Debug],
+    'server/alice/friend': [access.Read, access.Write],
+    'server/che': [access.Read, access.Write],
+    'server/che/friend': [access.Read],
+    '': [access.Read],
   };
 
   for (var name in expectations) {
@@ -74,20 +83,102 @@ test('full suite of go tests', function(assert) {
     };
     var exp = expectations[name];
 
-    for (var l in Labels) {
-      if (!Labels.hasOwnProperty(l)) {
-        continue;
-      }
-      var shouldErr = exp.indexOf(Labels[l]) === -1;
-      ctx.label = Labels[l];
+    for (var j = 0; j < allTags.length; j++) {
+      var tag = allTags[j];
+      var shouldErr = exp.indexOf(tag) === -1;
+      ctx.methodTags = [tag];
       var err = auth(ctx);
       if (shouldErr) {
-        assert.ok(err !== null, 'name: ' + name + ', l: ' + l);
-        assert.ok(err instanceof verror.NoAccessError);
+        assert.ok(err !== null, 'name: ' + name + ', tag: ' + tag);
+        assert.ok(err instanceof access.NoPermissionsError);
       } else {
-        assert.equal(err, null, 'name ' + name + ', l:' + l);
+        assert.equal(err, null, 'name ' + name + ', tag:' + tag);
       }
     }
   }
+  assert.end();
+});
+
+test('tags of different types', function(assert) {
+  var MyTag = createConstructor({
+    kind: kind.STRING,
+    name: 'MyTag'
+  });
+  var myAdmin = new MyTag('Admin');
+  var ctx = {
+    localBlessings: {
+      publicKey: 'me',
+    },
+    remoteBlessings: {
+      publicKey: 'otherkey',
+    },
+    remoteBlessingStrings: ['server/alice', 'server/bob/friend'],
+  };
+
+  var acl = new Map();
+  acl.set('Admin', {
+    in: ['server/alice'],
+    notIn: []
+  });
+  var tagAuthorizer = aclAuthorizer(acl, access.Tag);
+  ctx.methodTags = [myAdmin, access.Resolve];
+  var err = tagAuthorizer(ctx);
+  assert.ok(err !== null);
+  assert.ok(err instanceof access.NoPermissionsError);
+  var myTagAuthorizer = aclAuthorizer(acl, MyTag);
+  assert.equal(myTagAuthorizer(ctx), null);
+  assert.end();
+});
+
+test('no tags of a type', function(assert) {
+  var MyTag = createConstructor({
+    kind: kind.STRING,
+    name: 'MyTag'
+  });
+  var myAdmin = new MyTag('Admin');
+  var ctx = {
+    localBlessings: {
+      publicKey: 'me',
+    },
+    remoteBlessings: {
+      publicKey: 'otherkey',
+    },
+    remoteBlessingStrings: ['server/alice', 'server/bob/friend'],
+  };
+
+  var acl = new Map();
+  acl.set('Admin', {
+    in: ['server/alice'],
+    notIn: []
+  });
+  var tagAuthorizer = aclAuthorizer(acl, access.Tag);
+  ctx.methodTags = [myAdmin];
+  var err = tagAuthorizer(ctx);
+  assert.ok(err !== null);
+  assert.equal(err.id, 'v.io/v23/security/access.errNoMethodTags');
+  assert.end();
+});
+
+test('multiple tags of a type', function(assert) {
+  var ctx = {
+    localBlessings: {
+      publicKey: 'me',
+    },
+    remoteBlessings: {
+      publicKey: 'otherkey',
+    },
+    remoteBlessingStrings: ['server/alice', 'server/bob/friend'],
+  };
+
+  var acl = new Map();
+  acl.set('Admin', {
+    in: ['server/alice'],
+    notIn: []
+  });
+  var tagAuthorizer = aclAuthorizer(acl, access.Tag);
+  ctx.methodTags = [access.Resolve, access.Debug];
+  var err = tagAuthorizer(ctx);
+  assert.ok(err !== null);
+  assert.equal(err.id, 'v.io/v23/security/access.errMultipleMethodTags');
   assert.end();
 });
