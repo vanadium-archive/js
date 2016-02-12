543cd73bd8e6053d5d720c4936ac7f4af5d4a7bb
Benjamin Prosnitz <bprosnitz@google.com>
2016-02-12 15:10:07 -0800

    TBR js: add output to diagnose js-vdl date failure
    Change-Id: Ib5fc1b9901b3c227490cdd41b168de578e0395af

--- test/vdl/test-vdl-native.js
+++ test/vdl/test-vdl-native.js
@@ -9,6 +9,7 @@ test('time constants are Dates', function(assert) {
   var expectedTime = new Date(2012,11,15, 8,15,20, 453);
   assert.ok(time.D instanceof Date, 'constant should be a date');
   var diff = Math.abs(expectedTime - time.D);
-  assert.ok(diff < 1, 'constant should be right date');
+  assert.ok(diff < 1, 'constant should be right date got' + time.D +
+   ' expected ' + expectedTime + ' (diff was ' + diff + ')');
   assert.end();
 });
